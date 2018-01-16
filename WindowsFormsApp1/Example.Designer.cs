aafd4761f6a6ea9757621017c22edb9f8d792b2c
arefiev1997 <a.arefiev97@gmail.com>
2018-01-16 22:37:33 +0400

    example is good

--- WindowsFormsApp1/Example.Designer.cs
+++ WindowsFormsApp1/Example.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Example));
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,34 +43,37 @@
             // 
             // button4
             // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(3, 193);
+            this.button4.Location = new System.Drawing.Point(3, 253);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(416, 92);
+            this.button4.Size = new System.Drawing.Size(239, 119);
             this.button4.TabIndex = 16;
-            this.button4.Text = "Deserialize";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Location = new System.Drawing.Point(3, 98);
+            this.button3.Location = new System.Drawing.Point(3, 128);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(416, 89);
+            this.button3.Size = new System.Drawing.Size(239, 119);
             this.button3.TabIndex = 15;
-            this.button3.Text = "serialize";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button2.Location = new System.Drawing.Point(3, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(416, 89);
+            this.button2.Size = new System.Drawing.Size(239, 119);
             this.button2.TabIndex = 14;
-            this.button2.Text = "Xlsx";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -84,10 +88,11 @@
             // 
             // customGroupBox1
             // 
-            this.customGroupBox1.BackColorChart = System.Drawing.Color.White;
-            this.customGroupBox1.BackColorTree = System.Drawing.SystemColors.Control;
-            this.customGroupBox1.BorderlineColorChart = System.Drawing.Color.White;
-            this.customGroupBox1.BorderlineDashStyleChart = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            this.customGroupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.customGroupBox1.BackColorChart = System.Drawing.Color.LightYellow;
+            this.customGroupBox1.BackColorTree = System.Drawing.SystemColors.InactiveCaption;
+            this.customGroupBox1.BorderlineColorChart = System.Drawing.Color.Maroon;
+            this.customGroupBox1.BorderlineDashStyleChart = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
             this.customGroupBox1.BorderlineWidthChart = 1;
             this.customGroupBox1.BorderStyleTree = System.Windows.Forms.BorderStyle.Fixed3D;
             this.customGroupBox1.ColumnCount = 2;
@@ -97,8 +102,14 @@
             this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.customGroupBox1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.customGroupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customGroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.customGroupBox1.FontTree = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.customGroupBox1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
             this.customGroupBox1.Location = new System.Drawing.Point(3, 3);
             this.customGroupBox1.Name = "customGroupBox1";
             this.customGroupBox1.RootName = "Chart Tree";
@@ -106,16 +117,18 @@
             this.customGroupBox1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.customGroupBox1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.customGroupBox1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.customGroupBox1.Size = new System.Drawing.Size(422, 288);
+            this.customGroupBox1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.customGroupBox1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.customGroupBox1.Size = new System.Drawing.Size(745, 375);
             this.customGroupBox1.TabIndex = 8;
-            this.customGroupBox1.TypeOfChart = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            this.customGroupBox1.TypeOfChart = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar;
             this.customGroupBox1.WidthChart = 50F;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.Controls.Add(this.customGroupBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -123,7 +136,7 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(856, 294);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1002, 381);
             this.tableLayoutPanel1.TabIndex = 18;
             // 
             // tableLayoutPanel2
@@ -134,21 +147,22 @@
             this.tableLayoutPanel2.Controls.Add(this.button4, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.button3, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(431, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(754, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(422, 288);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(245, 375);
             this.tableLayoutPanel2.TabIndex = 9;
             // 
             // Example
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 294);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1002, 381);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Example";
